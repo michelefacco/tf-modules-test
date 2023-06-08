ddba98e0d9019ee865a9c47c25ea96091010446e
michelefacco <Michele.Facco@lexisnexisrisk.com>
2023-06-08 11:44:49 +0100

    fixing modules refs

--- ws-s3-submodule-test/main.tf
+++ ws-s3-submodule-test/main.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "git@github.com:michelefacco/tf-modules-test.git//ws-s3-submodule-test"
+  source = "git@github.com:michelefacco/tf-modules-test.git//ws-s3-module-test"
   
   bucket_name = var.bucket_name
 
