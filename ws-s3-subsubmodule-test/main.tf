fca4404837bfa742bd6fe5b8cc3609d23dff3492
michelefacco <Michele.Facco@lexisnexisrisk.com>
2023-06-14 16:46:22 +0100

    trying to move everything to the module

--- ws-s3-subsubmodule-test/main.tf
+++ ws-s3-subsubmodule-test/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "s3_bucket" {
   source = "git@github.com:michelefacco/tf-modules-test.git//ws-s3-submodule-test"
 
